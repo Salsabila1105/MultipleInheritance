02451406025dedcc71b5d2a5ad467e4ad68fa32e
Salsabila1105 <75847941+Salsabila1105@users.noreply.github.com>
2021-12-24 16:12:12 +0000

    class

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 class Ricecooker:
+  def __init__(self,nama_merk,tuas,mode): 
+    self.nama_merk = merk
+    self.tuas = tuas 
+    self.mode = mode
+
+class ModernRicecooker:
+  def __init__(self,temperature):
+    self.temperature = temperature
+    
+    
