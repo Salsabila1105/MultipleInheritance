89439eeba632c67b73c21654884122a74992ca45
Salsabila1105 <75847941+Salsabila1105@users.noreply.github.com>
2021-12-26 10:04:34 +0000

    set temperature

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 class Ricecooker:
   def __init__(self,nama_merk,tuas,mode): 
-    self.nama_merk = merk
+    self.nama_merk = nama_merk
     self.tuas = tuas 
     self.mode = mode
 
-class ModernRicecooker:
+class TypeRicecooker:
   def __init__(self,temperature):
     self.temperature = temperature
+    self.temperaturesekarang = self.temperature + 50
     
-    
+class ModernRicecooker: 
+  def __init__(self,nama_merk,tuas,mode,temperature):
+    Ricecooker.__init__(self,nama_merk,tuas,mode)
+    TypeRicecooker.__init__(self, temperature)
+
+YoungMa= ModernRicecooker('YoungMa', 'ON', 'Cook',100)
+print(YoungMa.nama_merk)
+print(YoungMa.tuas)
+print(YoungMa.mode)
+print(YoungMa.temperature)
+print(YoungMa.temperaturesekarang)
